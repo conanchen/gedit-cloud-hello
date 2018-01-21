4036098723347e7b8ed1fe1fc0f3bbe831fc0a30
conanchen <conan8chan@yahoo.com>
2018-01-21 12:43:23 +0800

    hello list sample

--- src/main/java/com/github/conanchen/gedit/hello/grpc/HelloService.java
+++ src/main/java/com/github/conanchen/gedit/hello/grpc/HelloService.java
@@ -7,7 +7,9 @@ import org.lognet.springboot.grpc.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 @GRpcService(interceptors = {LogInterceptor.class})
@@ -30,6 +32,31 @@ public class HelloService extends HelloGrpc.HelloImplBase {
         HelloReply helloReply = replyBuilder.build();
         responseObserver.onNext(helloReply);
         log.info(String.format("HelloService.sayHello() %s:%s gson=%s", helloReply.getUuid(), helloReply.getMessage(), gson.toJson(helloReply)));
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void listOldHello(ListHelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        for (int i = 0; i < request.getSize(); i++) {
+            String uuid = UUID.randomUUID().toString();
+            final HelloReply.Builder replyBuilder = HelloReply.newBuilder()
+                    .setStatus(Status.newBuilder()
+                            .setCode(io.grpc.Status.Code.OK.name())
+                            .setDetails("Hello很高兴回复你，你的hello很温暖@" + DateFormat.getDateTimeInstance().format(new Date()))
+                            .build())
+                    .setUuid(uuid)
+                    .setMessage(String.format("Hello world%s@%s ", uuid, DateFormat.getDateTimeInstance().format(new Date())))
+                    .setCreated(System.currentTimeMillis())
+                    .setLastUpdated(System.currentTimeMillis());
+            HelloReply helloReply = replyBuilder.build();
+            responseObserver.onNext(helloReply);
+            try {
+                Thread.sleep(300);
+                log.info("sleep{} to send {}th hello", 300, i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         responseObserver.onCompleted();
     }
 }
